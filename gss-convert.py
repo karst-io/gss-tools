516206ec07861fa7823c0fd1e443e8899a4f16d1
Will Urbanski <will.urbanski@gmail.com>
2015-12-29 16:20:02 -0500

    handle multiple entrances

--- gss-convert.py
+++ gss-convert.py
@@ -68,127 +68,132 @@ for line in fptr.readlines():
 
 			cave_file_path = "%s/%s.json" % (output_dir, code)
 
+			#create new one
+			entrance_obj = 	{
+								'id': str(uuid.uuid4()),
+								'name': name,
+								'coordinates': [
+									{
+										'id': str(uuid.uuid4()),
+										'system': 'NAD27',
+										'format': 'DMS',
+										'latitude': lat,
+										'longitude': lon,
+										'accuracy': accuracy_dict[accuracy],
+									}
+								],
+								'elevation': elevation,
+								'status': 'UNKNOWN',
+								'ownership': 'UNKNOWN',
+								'type': 'UNKNOWN',
+								'negotiation': 'UNKNOWN',
+								'formation': 'NATURAL',
+								'location': 'UNKNOWN',
+								'characteristics': 'UNKNOWN',
+								'hydrology': 'UNKNOWN',
+								'hazards': 'UNKNOWN',
+							}
+
+			if entry_status == 'B':
+				#BIOLOGICALLY SIGNIFICANT; ASK PERMISSION
+				entrance_obj['status'] = 'PERMISSION REQUIRED',
+				entrance_obj['status_description'] = 'Biologically significant; ask permission'
+			elif entry_status == 'C':
+				#commercial cave
+				entrance_obj['ownership'] = 'COMMERCIAL'
+			elif entry_status == 'D':
+				#govt
+				entrance_obj['status'] = 'DESTROYED'
+			elif entry_status == 'G' or entry_status == 'K':
+				#govt park
+				entrance_obj['ownership'] = 'GOVERNMENT'
+			elif entry_status == 'L':
+				#locked/gated
+				entrance_obj['status'] = 'LOCKED / GATED'
+			elif entry_status == 'N':
+				#forbidden
+				entrance_obj['status'] = 'FORBIDDEN'
+			elif entry_status == 'P':
+				#private
+				entrance_obj['ownership'] = 'PRIVATE PROPERTY'
+			elif entry_status == 'S':
+				#NSS
+				entrance_obj['ownership'] = 'CONSERVANCY'
+			else:
+				throw_warning(line, "Unknown entry status %s" % entry_status)
+
+			if entrance_type == 'E' or entrance_type == 'L':
+				entrance_obj['type'] = 'HORIZONTAL'
+				entrance_obj['type_description'] = 'extremely big, over 20 feet wide and/or high'
+				entrance_obj['formation'] = 'NATURAL'
+				entrance_obj['negotiation'] = 'WALK'
+			elif entrance_type == 'F':
+				entrance_obj['type'] = 'HORIZONTAL'
+				entrance_obj['negotation'] = 'SWIM'
+				entrance_obj['hydrology'] = 'SUBMERGED'
+			elif entrance_type == 'H':
+				entrance_obj['type'] = 'HORIZONTAL'
+				entrance_obj['type_description'] = 'stoop or duck walk'
+				entrance_obj['negotiation'] = 'STOOP'
+			elif entrance_type == 'K':
+				entrance_obj['type'] = 'HORIZONTAL'
+				entrance_obj['negotiation'] = 'CRAWL'
+			elif entrance_type == 'T':
+				entrance_obj['type'] = 'HORIZONTAL'
+				entrance_obj['formation'] = 'ARTIFICIAL'
+			elif entrance_type == 'A':
+				entrance_obj['type'] = 'VERTICAL'
+				entrance_obj['formation'] = 'ARTIFICIAL'
+			elif entrance_type == 'B':
+				entrance_obj['type'] = 'VERTICAL'
+				entrance_obj['negotiation_description'] = 'bottleneck (small but bells out)'
+			elif entrance_type == 'C':
+				entrance_obj['type'] = 'VERTICAL'
+				entrance_obj['negotiation'] = 'CLIMB'
+				entrance_obj['negotiation_description'] = 'chimney or climb-down'
+			elif entrance_type == 'O':
+				entrance_obj['type'] = 'VERTICAL'
+				entrance_obj['negotiation'] = 'RAPPEL'
+			elif entrance_type == 'P':
+				entrance_obj['type'] = 'VERTICAL'
+				entrance_obj['negotiation'] = 'RAPPEL'
+			else:
+				throw_warning(line, "unknown entrance type '%s'" % entrance_type)
+
+
+			if field_indication == 'B':
+				entrance_obj['field_indication'] = 'BLUFF OR HEADWALL'
+			elif field_indication == 'H':
+				entrance_obj['field_indication'] = 'HILLSIDE'
+			elif field_indication == 'I':
+				entrance_obj['hydrology'] = 'WATER IN'
+			elif field_indication == 'L':
+				entrance_obj['field_indication'] = 'BENCH'
+			elif field_indication == 'P':
+				entrance_obj['hydrology'] = 'LAKE OR POOL'
+				entrance_obj['field_indication'] = 'SINK'
+			elif field_indication == 'Q':
+				entrance_obj['field_indication'] = 'QUARRY'
+			elif field_indication == 'R':
+				entrance_obj['field_indication'] = 'ROAD CUT'
+			elif field_indication == 'S':
+				entrance_obj['field_indication'] = 'SINK'
+				entrance_obj['hydrology'] = 'NONE'
+			elif field_indication == 'W':
+				entrance_obj['field_indication'] = 'STREAMBED'
+			elif field_indication == 'X':
+				entrance_obj['hydrology'] = 'WATER OUT'
+
 			if os.path.exists(cave_file_path) == True:
-				#one already exists
+				#one already exists; load it
 				fptr = open(cave_file_path,'r')
 				cave_obj = json.loads(fptr.read())
+
+				cave_obj['entrances'].append(entrance_obj)
+
 			else:
-				#create new one
-				entrance_obj = 	{
-									'id': str(uuid.uuid4()),
-									'name': name,
-									'coordinates': [
-										{
-											'id': str(uuid.uuid4()),
-											'system': 'NAD27',
-											'format': 'DMS',
-											'latitude': lat,
-											'longitude': lon,
-											'accuracy': accuracy_dict[accuracy],
-										}
-									],
-									'elevation': elevation,
-									'status': 'UNKNOWN',
-									'ownership': 'UNKNOWN',
-									'type': 'UNKNOWN',
-									'negotiation': 'UNKNOWN',
-									'formation': 'NATURAL',
-									'location': 'UNKNOWN',
-									'characteristics': 'UNKNOWN',
-									'hydrology': 'UNKNOWN',
-									'hazards': 'UNKNOWN',
-								}
-
-				if entry_status == 'B':
-					#BIOLOGICALLY SIGNIFICANT; ASK PERMISSION
-					entrance_obj['status'] = 'PERMISSION REQUIRED',
-					entrance_obj['status_description'] = 'Biologically significant; ask permission'
-				elif entry_status == 'C':
-					#commercial cave
-					entrance_obj['ownership'] = 'COMMERCIAL'
-				elif entry_status == 'D':
-					#govt
-					entrance_obj['status'] = 'DESTROYED'
-				elif entry_status == 'G' or entry_status == 'K':
-					#govt park
-					entrance_obj['ownership'] = 'GOVERNMENT'
-				elif entry_status == 'L':
-					#locked/gated
-					entrance_obj['status'] = 'LOCKED / GATED'
-				elif entry_status == 'N':
-					#forbidden
-					entrance_obj['status'] = 'FORBIDDEN'
-				elif entry_status == 'P':
-					#private
-					entrance_obj['ownership'] = 'PRIVATE PROPERTY'
-				elif entry_status == 'S':
-					#NSS
-					entrance_obj['ownership'] = 'CONSERVANCY'
-				else:
-					throw_warning(line, "Unknown entry status %s" % entry_status)
-
-				if entrance_type == 'E' or entrance_type == 'L':
-					entrance_obj['type'] = 'HORIZONTAL'
-					entrance_obj['type_description'] = 'extremely big, over 20 feet wide and/or high'
-					entrance_obj['formation'] = 'NATURAL'
-					entrance_obj['negotiation'] = 'WALK'
-				elif entrance_type == 'F':
-					entrance_obj['type'] = 'HORIZONTAL'
-					entrance_obj['negotation'] = 'SWIM'
-					entrance_obj['hydrology'] = 'SUBMERGED'
-				elif entrance_type == 'H':
-					entrance_obj['type'] = 'HORIZONTAL'
-					entrance_obj['type_description'] = 'stoop or duck walk'
-					entrance_obj['negotiation'] = 'STOOP'
-				elif entrance_type == 'K':
-					entrance_obj['type'] = 'HORIZONTAL'
-					entrance_obj['negotiation'] = 'CRAWL'
-				elif entrance_type == 'T':
-					entrance_obj['type'] = 'HORIZONTAL'
-					entrance_obj['formation'] = 'ARTIFICIAL'
-				elif entrance_type == 'A':
-					entrance_obj['type'] = 'VERTICAL'
-					entrance_obj['formation'] = 'ARTIFICIAL'
-				elif entrance_type == 'B':
-					entrance_obj['type'] = 'VERTICAL'
-					entrance_obj['negotiation_description'] = 'bottleneck (small but bells out)'
-				elif entrance_type == 'C':
-					entrance_obj['type'] = 'VERTICAL'
-					entrance_obj['negotiation'] = 'CLIMB'
-					entrance_obj['negotiation_description'] = 'chimney or climb-down'
-				elif entrance_type == 'O':
-					entrance_obj['type'] = 'VERTICAL'
-					entrance_obj['negotiation'] = 'RAPPEL'
-				elif entrance_type == 'P':
-					entrance_obj['type'] = 'VERTICAL'
-					entrance_obj['negotiation'] = 'RAPPEL'
-				else:
-					throw_warning(line, "unknown entrance type '%s'" % entrance_type)
-
-
-				if field_indication == 'B':
-					entrance_obj['field_indication'] = 'BLUFF OR HEADWALL'
-				elif field_indication == 'H':
-					entrance_obj['field_indication'] = 'HILLSIDE'
-				elif field_indication == 'I':
-					entrance_obj['hydrology'] = 'WATER IN'
-				elif field_indication == 'L':
-					entrance_obj['field_indication'] = 'BENCH'
-				elif field_indication == 'P':
-					entrance_obj['hydrology'] = 'LAKE OR POOL'
-					entrance_obj['field_indication'] = 'SINK'
-				elif field_indication == 'Q':
-					entrance_obj['field_indication'] = 'QUARRY'
-				elif field_indication == 'R':
-					entrance_obj['field_indication'] = 'ROAD CUT'
-				elif field_indication == 'S':
-					entrance_obj['field_indication'] = 'SINK'
-					entrance_obj['hydrology'] = 'NONE'
-				elif field_indication == 'W':
-					entrance_obj['field_indication'] = 'STREAMBED'
-				elif field_indication == 'X':
-					entrance_obj['hydrology'] = 'WATER OUT'
-
+				
+				#cave does not exist; create a new cave object
 				cave_obj = 	{
 							'id': str(uuid.uuid4()), 
 							'code': code, 
@@ -198,7 +203,10 @@ for line in fptr.readlines():
 							'depth': depth
 							}
 
-				fptr = open(cave_file_path,'w')
-				fptr.write(json.dumps(cave_obj))
-				fptr.close()
+			#save output
+			fptr = open(cave_file_path,'w')
+			fptr.write(json.dumps(cave_obj))
+			fptr.close()
+		else:
+			print "WARN: %s" % record_type
 
